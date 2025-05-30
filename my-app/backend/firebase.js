b961168ee2fa6c57540b9c9a35b0cd7d3dca5909
Kgbadao <kgbadamosi2015@gmail.com>
2025-05-30 20:04:26 +0300

    Update firebase.js

--- my-app/backend/firebase.js
+++ my-app/backend/firebase.js
@@ -1,10 +1,1 @@
-// firebase.js
-import admin from 'firebase-admin';
-import serviceAccount from './telemedical-projeect.json' with { type: "json" };
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://your-project-id.firebaseio.com'
-});
-
-export const db = admin.firestore();
