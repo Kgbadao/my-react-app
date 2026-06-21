e347628b41d2bb591396fbe58e78e50514b18f11
Kgbdao <kgbadamosi2015@gmail.com>
2026-06-21 20:10:05 +0100

    Updated admin approval

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -46,7 +46,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-secret'],
 }));
 
 app.options(/.*/, cors());
